85ea0066e689eca0671a610e430f237fadb00cd8
djavadihamid <djavadi.hamid@yandex.com>
2020-07-17 08:23:32 -0700

    [FEAT] editorSingleton has been added for editor scripts

---
+++ Assets/Scripts/Singletons/EditorSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Modules.Singleton
+{
+    public class EditorSingleton<T> : UnityEditor.Editor where T : class, new()
+    {
+        private static Lazy<T> _ins = new Lazy<T>(() => new T());
+        public static T Ins => _ins.Value;
+
+        protected EditorSingleton()
+        {
+            Init();
+        }
+
+        protected virtual void Init()
+        {
+        }
+
+        protected void ResetInstance()
+        {
+            _ins = new Lazy<T>(() => new T());
+        }
+    }
+}
