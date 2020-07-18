c1ae88c0063a389114a23663bfe6fc1afef6e860
djavadihamid <djavadi.hamid@yandex.com>
2020-07-18 04:38:38 -0700

    [FEAT] singleton new persisten gameobject with wanted comonent

---
+++ Assets/Scripts/Singletons/SingletonPersistentNewGameobject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class SingletonPersistentNewGameobject<T> : MonoBehaviour where T : MonoBehaviour, new()
+{
+    private static Lazy<T> _ins = new Lazy<T>(() => { return new GameObject(typeof(T).Name).AddComponent<T>(); });
+    public static T Ins => _ins.Value;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    protected void ResetInstance()
+    {
+        _ins = new Lazy<T>(() => { return new GameObject(typeof(T).Name).AddComponent<T>(); });
+    }
+}
